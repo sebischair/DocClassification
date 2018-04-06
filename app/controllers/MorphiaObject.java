65a726e806a42d8170f8028513aad11cd5d786e1
Jiping Xu <xujiping2003@gmail.com>
2018-04-06 21:55:14 +0200

    #1: connect to database without credentials in docker

--- app/controllers/MorphiaObject.java
+++ app/controllers/MorphiaObject.java
@@ -14,6 +14,7 @@ public class MorphiaObject {
     static public Datastore datastore;
 
     static public void connect(String dbUrl, Integer dbPort, String dbName, String dbUsername, String dbPwd) throws Exception{
+        String dockerHost = "mongo";    // For docker, don't provide credentials to database
         ServerAddress sa = new ServerAddress(dbUrl, dbPort);
         List<MongoCredential> cl = new ArrayList<>();
         MongoCredential mc = MongoCredential.createCredential(dbUsername, dbName, dbPwd.toCharArray());
@@ -21,7 +22,11 @@ public class MorphiaObject {
 
         morphia = new Morphia();
         morphia.mapPackage("app.model");
-        datastore = morphia.createDatastore(new MongoClient(sa, cl), dbName);
+        if (dbUrl.equals(dockerHost)) {
+            datastore = morphia.createDatastore(new MongoClient(sa), dbName);
+        } else {
+            datastore = morphia.createDatastore(new MongoClient(sa, cl), dbName);
+        }
         datastore.ensureIndexes();
         datastore.ensureCaps();
     }
